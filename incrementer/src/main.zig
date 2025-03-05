383f5b17570c87f7d53d54bbe56e551807456713
Sandwich <299465+dskvr@users.noreply.github.com>
2025-03-06 00:45:16 +0700

    remove old code

--- incrementer/src/main.zig
+++
@@ -1,9 +0,0 @@
-export var memory: [65536]u8 align(65536) = undefined;
-
-export fn increment(value: i32) i32 {
-    return value + 1;
-}
-
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
