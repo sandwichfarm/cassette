bff340a0bc226fc477a93314a7d4225e6d8743c5
Sandwich <299465+dskvr@users.noreply.github.com>
2025-03-06 21:23:36 +0700

    cleanup

--- cassette/src/main.zig
+++ cassette/src/main.zig
@@ -1,12 +1,10 @@
 pub const schema = @import("schema.zig");
 pub const auto_describe = @import("auto_describe.zig");
 
-// Re-export all schema functionality
 pub const getTypeName = schema.getTypeName;
 pub const generateFunctionSchema = schema.generateFunctionSchema;
 pub const createWasmString = schema.createWasmString;
 pub const createDescribeFunction = schema.createDescribeFunction;
 pub const createSchemaGetter = schema.createSchemaGetter;
 
-// Re-export auto_describe functionality
 pub const generateDescribeFunction = auto_describe.generateDescribeFunction;
