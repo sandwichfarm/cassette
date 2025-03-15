48d4de522b1086d17320a13cac369d1079867154
Paul Miller <paul@paul.lol>
2025-03-15 06:23:34 -0500

    delete everything again

--- plusone-wasm/src/main.zig
+++
@@ -1,9 +0,0 @@
-export fn increment(value: i32) i32 {
-    return value + 1;
-}
-
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-export var memory: [65536]u8 align(65536) = undefined;
